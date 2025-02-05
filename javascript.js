798fbf836df3aebeadc49966f62462a07e6c766e
Mrdedsecurity <Jordanp20@live.co.uk>
2025-02-05 17:04:05 +0000

    XSS in search in progress

--- javascript.js
+++ javascript.js
@@ -6,3 +6,20 @@ hintButtons.forEach(button => {
         hint.style.display = hint.style.display === 'none' ? 'block' : 'none';
     });
 });
+
+const searchInput = document.getElementById('searchInput');
+const searchButton = document.getElementById('searchButton');
+const cards = document.querySelectorAll('.card');
+
+searchButton.addEventListener('click', () => {
+    const searchTerm = searchInput.value; // Remove toLowerCase() for XSS vulnerability
+
+    cards.forEach(card => {
+        const companyName = card.querySelector('.card-body p:first-child strong').textContent; 
+        if (companyName.includes(searchTerm)) {
+            card.style.display = 'block';
+        } else {
+            card.style.display = 'none';
+        }
+    });
+});
