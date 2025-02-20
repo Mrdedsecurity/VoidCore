4236a5cf94faad53a194ece6c3f4fc9e4da59e99
Mrdedsecurity <Jordanp20@live.co.uk>
2025-02-20 16:02:47 +0000

    SQL injection in progress, VoidSoup profile added, need to fix css for profile.

---
+++ login.php
@@ -0,0 +1,35 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "voidcore";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $user = $_POST['username'];
+    $pass = $_POST['password'];
+
+    
+    $sql = "SELECT * FROM users WHERE username = '$user' AND password = '$pass'";
+    $result = $conn->query($sql);
+
+    if ($result->num_rows > 0) {
+        $_SESSION['username'] = $username;
+        
+        // Redirect "VoidSoup" to VoidSoup.php
+        if ($username === "VoidSoup") {
+            header("Location: VoidSoup.php");
+            exit();
+        } else {
+            echo "Login successful! But only VoidSoup has access to special areas.";
+        }
+    } else {
+        echo "Invalid credentials.";
+    }
+}
+?>
